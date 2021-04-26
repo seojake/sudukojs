10e87c030d53cc5c5ea028dbc590243781dbb572
seojake <jake@seojake.com>
2021-04-26 20:45:48 +0100

    Starting on valid cols and rows

--- app.js
+++ app.js
@@ -16,15 +16,42 @@ create_board();
  * Create board
  * Will create a working Suduko board and returns the array
  */
-function create_board(){
+function create_board() {
     var group_size = board_size / 3;
     var board = [];
 
-    for(column = 0; column < board_size; column++){
-        board.push([]);
+    // Check the column and row if that number had been used
+    var is_valid = function (n, row, column) {
+        // Check the column
+        // if (board[column].includes(n)) {
+        //     return false;
+        // }
+
+        // Check the row
+        if (board[row].includes(n)) {
+            return false;
+        }
+
+        return true;
     }
 
-    board.forEach((cell) => {
-        
-    });
+    // Start to create the board
+    for (row = 0; row < board_size; row++) {
+        board.push([]);
+        for (column = 0; column < board_size; column++) {
+            while (true) {
+                var number = Math.ceil((Math.random() * 9));
+                if (is_valid(number, row, column)) {
+                    board[row][column] = number;
+                    break;
+                }
+            }
+        }
+    }
+
+    console.log(board);
 }
+
+// function run_checks(column, row) {
+
+// }
